0651a3fff704a039ee69ce846289a45560d290c1
TaniBarua <70139682+TaniBarua@users.noreply.github.com>
2020-10-09 20:01:19 +0600

    Add files via upload

---
+++ buy.php
@@ -0,0 +1,55 @@
+<?php include("includes/header.php") ?>
+<?php include("includes/navbar.php") ?>
+    
+
+  <div class="alert alert-light" role="alert">
+    <div class="row">
+                       
+                        <div class="col-md-6">
+                         <h5  style="color: black">Cash on Delivery after receiving your product at your home!</h5>
+
+  <p  style="color: black"> Your product delivered at your home and then pay cash to our delivery person. No advance payment is required.</p>
+  
+  <h5  style="color: black">Enjoy the easy payment option of bKash!</h5>
+  
+  <h5 style="color: black">Payment process</h5>
+  
+  <p  style="color: black">Go to bKash Mobile Menu by dialing *247#</p>
+  <p style="color: black">Choose 'Payment</br></p>
+  <p style="color: black"> the Account Number; 01717864118</br></p>
+    <p style="color: black">Enter Bill Amount</br></p>
+    <p style="color: black"> Enter a reference against your payment (Your Name/Phone Number)</br></p>
+    <p style="color: black">Enter the counter number: 1</br></p>
+    <p style="color: black">Now enter your bKash PIN to confirm Done!</br></p>
+    <p style="color: black">You will receive a confirmation message from bKash</br></p>
+    <p style="color: black">Please, bKash pay to 01717864118</br></p>
+    <p style="color: black">bKash payment system for daraz.com</p>
+  </br>
+   
+  <h5 style="color: black">Pay using your Internet Banking and enjoy safe shopping!<br></h5>
+  
+  <h5 style="color: black"> Pay through the convenient, speedy and secure net banking. You can deposit online or directly to: Account Name:
+   A/C Name:DARAZ.com; No: 106-129-0391-743, Eastern Bank Limited, any branch. No cash handling risk!
+  </h5>
+  </div>
+  </div>
+  </div>
+     
+<div class="footer" style="background-color:rgb(0,0,74)">
+
+<h4><p style="color:background-color:rgb(0,0,74)" align="center">...</h4>
+
+ <h4><p style="color:white" align="center">DARAZ</h4>
+
+ <p style="color:white" align="center">Call Now:01816781772</p>
+ <p style="color:white" align="center">Email:info@retinabd.com</p>
+ <p style="color:white" align="center">c2015-2019 All Right Reserved</p>
+
+ <h4><p style="color:background-color:rgb(0,0,74)" align="center">...</h4>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
